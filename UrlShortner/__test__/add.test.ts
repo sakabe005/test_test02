44f757b233822a3135c492fbaf600316247da233
sakabe005 <sakabe1201@gmail.com>
2024-08-12 23:25:22 +0900

    add small change

--- UrlShortner/__test__/add.test.ts
+++ UrlShortner/__test__/add.test.ts
@@ -3,10 +3,10 @@ import { test, expect, describe } from '@jest/globals';
 
 describe('add', () => {
   test('add 2 + 4', () => {
-    expect(add(2, 4)).toBe(6);
+    expect(add(2, 4, 5)).toBe(11);
   });
 
   test('doubles 5 + 5', () => {
-    expect(add(5, 5)).toBe(10);
+    expect(add(5, 5, 2)).toBe(12);
   });
 });
--- UrlShortner/add.js
+++ UrlShortner/add.js
@@ -1,3 +1,3 @@
-export function add(num1, num2) {
-    return num1 + num2;
+export function add(num1, num2, num3) {
+    return num1 + num2 + num3;
   }
