2a6546a32efedda8ce34099f10a2b5f66752a17b
sakabe1201@gmail.com <sakabe005>
2024-08-14 18:27:40 +0900

    delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/']
-};
